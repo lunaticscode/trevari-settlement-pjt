39a9e13756f3c858075779bc84f811e6b686089d
insoo <lunatics384@gmail.com>
2020-08-28 17:53:02 +0900

    [pjt-setting] front

---
+++ front/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        hot: true,
+        inline: true,
+        host: '0.0.0.0',
+        port: 9500,
+        contentBase: __dirname + '/public/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+
+                options: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin() ]
+};
